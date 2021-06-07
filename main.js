b8365e75bb6416dc68a25121348e625e84b242dc
Jacob Pitkin <jkpitkin@le.utah.gov>
2021-06-07 15:14:16 -0600

    Added linting for development and nodemon

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 const express = require('express');
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Express running...');
+  res.send('Express running...');
 });
 
-app.listen(port, () => console.log(`Express running on port ${port}`));
+app.listen(port, () => {
+  console.log(`Express running on port ${port}`);
+});
